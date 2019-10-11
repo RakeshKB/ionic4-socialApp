0720eb9f1eff7f426f6b9bf4e200242206b7bd44
rakeshkb@medibox.in <Shwetha#0309>
2019-10-11 15:27:43 +0530

    Integrating Firebase Auth Module

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjRIikXxtHxiuC6UlKQg1gssWtSNxJinU",
+    authDomain: "ionic4-socialapp.firebaseapp.com",
+    databaseURL: "https://ionic4-socialapp.firebaseio.com",
+    projectId: "ionic4-socialapp",
+    storageBucket: "ionic4-socialapp.appspot.com",
+    messagingSenderId: "214591782750",
+    appId: "1:214591782750:web:fe19236e3675b8f0c3d9cf"
+};
+
+export default firebaseConfig;
